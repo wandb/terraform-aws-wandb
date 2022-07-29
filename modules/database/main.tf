0878b5509bd9559a39806b8c48fad09921599592
Venkata <venkata@wandb.com>
2022-07-29 15:31:15 -0700

    revert minor version upgrade

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -11,7 +11,7 @@ resource "random_string" "master_password" {
 }
 
 locals {
-  is_mysql_80            = var.engine_version == "8.0.mysql_aurora.3.02.0"
+  is_mysql_80            = var.engine_version == "8.0.mysql_aurora.3.01.0"
   engine_version_tag     = local.is_mysql_80 ? "80" : "57"
   parameter_family       = local.is_mysql_80 ? "aurora-mysql8.0" : "aurora-mysql5.7"
   parameter_group_name   = "${var.namespace}-aurora-db-${local.engine_version_tag}-parameter-group"
