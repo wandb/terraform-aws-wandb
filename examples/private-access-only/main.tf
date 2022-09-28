5acef83c245abe0662289ea5ff6020fc58274915
Justin Brooks <jsbroks@gmail.com>
2022-09-28 23:48:25 +0000

    clean up example

--- examples/private-access-only/main.tf
+++ examples/private-access-only/main.tf
@@ -11,68 +11,36 @@ provider "aws" {
   }
 }
 
-module "networking" {
-  source    = "../../modules/networking"
-  namespace = var.namespace
-
-  amazon_side_asn = 64620
-
-  # Should be true if you want to create a new VPN Gateway resource and attach it to the VPC
-  enable_vpn_gateway = true
-  customer_gateways = {
-    IP1 = {
-      bgp_asn    = 65220
-      ip_address = "172.83.124.10"
-    }
-  }
-}
-
-module "vpn_gateway" {
-  source  = "terraform-aws-modules/vpn-gateway/aws"
-  version = "~> 2.0"
-
-  vpc_id              = module.networking.vpc_id
-  vpn_gateway_id      = module.networking.vgw_id
-  customer_gateway_id = module.networking.cgw_ids[0]
-
-  # precalculated length of module variable vpc_subnet_route_table_ids
-  vpc_subnet_route_table_count = 3
-  vpc_subnet_route_table_ids   = module.networking.private_route_table_ids
-
-  # tunnel inside cidr & preshared keys (optional)
-  # tunnel1_inside_cidr   = var.custom_tunnel1_inside_cidr
-  # tunnel2_inside_cidr   = var.custom_tunnel2_inside_cidr
-  # tunnel1_preshared_key = var.custom_tunnel1_preshared_key
-  # tunnel2_preshared_key = var.custom_tunnel2_preshared_key
-}
-
-
-resource "aws_route53_zone" "private" {
-  name = "wandb.internal"
-
-  vpc {
-    vpc_id = module.networking.vpc_id
-  }
-}
-
-module "standard" {
+module "wandb_infra" {
   source = "../../"
 
-  namespace     = var.namespace
-  public_access = false
+  namespace = var.namespace
 
-  wandb_license = var.wandb_license
+  public_access            = false
+  kubernetes_public_access = false
 
-  domain_name = aws_route53_zone.private.name
-  zone_id     = aws_route53_zone.private.zone_id
+  domain_name = var.domain
+}
 
-  # Creating a custom VPC so that we can initalize a route53 zone first and configure a vpn
-  create_vpc              = false
-  network_id              = module.networking.vpc_id
-  network_private_subnets = module.networking.private_subnets
-  network_public_subnets  = module.networking.public_subnets
+module "wandb_app" {
+  source = "github.com/wandb/terraform-kubernetes-wandb"
+
+  license = var.wandb_license
+
+  host                       = module.wandb_infra.url
+  bucket                     = "s3://${module.wandb_infra.bucket_name}"
+  bucket_aws_region          = module.wandb_infra.bucket_region
+  bucket_queue               = "sqs://${module.wandb_infra.bucket_queue_name}"
+  bucket_kms_key_arn         = module.wandb_infra.kms_key_arn
+  database_connection_string = "mysql://${module.wandb_infra.database_connection_string}"
+
+  service_port = module.wandb_infra.internal_app_port
+
+  # If we dont wait, tf will start trying to deploy while the work group is
+  # still spinning up
+  depends_on = [module.wandb_infra]
 }
 
 output "url" {
-  value = module.standard.url
+  value = module.wandb_infra.url
 }
