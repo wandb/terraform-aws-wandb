6019bca752906cf25a123cf166e9ab64f3fe1e46
Zachary Blasczyk <zb@Zachary-Blasczyk-TW7P70V256.local>
2023-01-03 14:00:11 -0600

    networking revert

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -4,7 +4,7 @@ data "aws_availability_zones" "available" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+  version = "~> 3.0"
 
   create_vpc = var.create_vpc
 
