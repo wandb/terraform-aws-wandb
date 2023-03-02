0990fd261aa06e3004ce4aa7b69616236c63516f
Justin Brooks <jsbroks@gmail.com>
2023-03-02 02:34:50 +0000

    fix replacement

--- scripts/set_variable_default.py
+++ scripts/set_variable_default.py
@@ -1,15 +1,18 @@
 import re
 import sys
 
+
 def set_variable_default(variable: str, value: str, content: str) -> str:
     """
     This script uses regex to replace the text. It makes an assumption that the
     default value must be declared before an conditions are applied.
     """
-    pat = '(variable\s+\"' + variable + '\"\s*{[^\}]+\s+default\s*=\s\").*(\"\n)'
+    pat = '(variable\s+\"' + variable + '\"\s*{[^}]+\s+default\s*=\s\").*(\"\n)'
+
+    rep = lambda m: f'{m.group(1)}{value}{m.group(2)}'
     return re.sub(
         pat,
-        r'\1' + value + r'\2',
+        rep,
         content
     )
 
@@ -19,7 +22,6 @@ if __name__ == '__main__':
     file_path = ''.join(var_file) or 'variables.tf'
 
     print(f"Setting \"{var}\" to \"{val}\" in \"{file_path}\"")
-
     with open(file_path, 'r+', encoding="utf8") as f:
         txt = f.read()
         result = set_variable_default(var, val, txt)
