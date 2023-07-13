701b886376007ca2726be3811e3e4037e21ee22d
George Scott <george.scott@wandb.com>
2023-07-13 18:39:43 -0500

    fix: Managed arns (#109)
    * managed arns --> attachments
    
    * Update iam-roles.tf

--- modules/app_eks/iam-roles.tf
+++ modules/app_eks/iam-roles.tf
@@ -1,8 +1,10 @@
 resource "aws_iam_role" "node" {
   name               = "${var.namespace}-node"
   assume_role_policy = data.aws_iam_policy_document.node_assume.json
-
+  managed_policy_arns = []
+  
   // todo: refactor --> v1.16.3
   inline_policy {}
 }
+
 
