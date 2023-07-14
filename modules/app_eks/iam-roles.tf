e473f16ab1a946f56ed23d70fb71430b02fd9f9b
George Scott <george.scott@wandb.com>
2023-07-13 19:29:51 -0500

    removed managed arns statement

--- modules/app_eks/iam-roles.tf
+++ modules/app_eks/iam-roles.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "node" {
   name               = "${var.namespace}-node"
   assume_role_policy = data.aws_iam_policy_document.node_assume.json
-  managed_policy_arns = []
+
   
   // todo: refactor --> v1.16.3
   inline_policy {}
