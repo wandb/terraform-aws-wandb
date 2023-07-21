19c1cd8ca8207b204c3b3b9f87d9234a35272fc5
George Scott <george.scott@wandb.com>
2023-07-21 14:36:17 -0500

    test

--- variables.tf
+++ variables.tf
@@ -12,11 +12,6 @@ variable "deletion_protection" {
   default     = true
 }
 
-variable "tags" {
-  nullable = false
-  type = map(string)
-}
-
 variable "use_internal_queue" {
   type    = bool
   default = false
