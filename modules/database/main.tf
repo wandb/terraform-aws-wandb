d27061e2e000643c08c6a8997b4b9e3ace5db576
George Scott <george.scott@wandb.com>
2023-07-24 10:16:05 -0500

    fix: Resolve duplicate tags issue (#112)
    * Removed commented-out lines.

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -103,7 +103,6 @@ module "aurora" {
   apply_immediately                     = true
   autoscaling_enabled                   = false
   backup_retention_period               = var.backup_retention_period
-  cluster_tags                          = { "Namespace" : "${var.namespace}" }
   create_db_subnet_group                = var.create_db_subnet_group
   create_random_password                = false
   create_security_group                 = true
