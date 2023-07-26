6426231c31e35e09fccede491fa091e8814f5c14
George Scott <george.scott@wandb.com>
2023-07-26 16:53:32 -0500

    Upgrade database provider

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -1,27 +1,19 @@
-locals {
-  redis_version = "6.x"
-}
-
 resource "aws_elasticache_replication_group" "default" {
-  replication_group_id          = "${var.namespace}-rep-group"
-  replication_group_description = "${var.namespace}-rep-group"
-  number_cache_clusters         = 2
-  port                          = 6379
-
-  node_type            = var.node_type
-  parameter_group_name = "default.redis6.x"
-  engine_version       = local.redis_version
-
-  automatic_failover_enabled = true
-  multi_az_enabled           = true
-  maintenance_window         = var.preferred_maintenance_window
-  snapshot_retention_limit   = 1
-
-  subnet_group_name  = var.redis_subnet_group_name
-  security_group_ids = [aws_security_group.redis.id]
-
-  kms_key_id                 = var.kms_key_arn
   at_rest_encryption_enabled = true
+  automatic_failover_enabled = true
+  description                = "${var.namespace}-rep-group"
+  engine_version             = "6.x"
+  kms_key_id                 = var.kms_key_arn
+  maintenance_window         = var.preferred_maintenance_window
+  multi_az_enabled           = true
+  node_type                  = var.node_type
+  num_cache_clusters         = 2
+  parameter_group_name       = "default.redis6.x"
+  port                       = 6379
+  replication_group_id       = "${var.namespace}-rep-group"
+  security_group_ids         = [aws_security_group.redis.id]
+  snapshot_retention_limit   = 1
+  subnet_group_name          = var.redis_subnet_group_name
   transit_encryption_enabled = true
 }
 
@@ -31,19 +23,19 @@ resource "aws_security_group" "redis" {
 }
 
 resource "aws_security_group_rule" "ingress" {
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = "6379"
-  to_port           = "6379"
   cidr_blocks       = var.vpc_subnets_cidr_blocks
+  from_port         = "6379"
+  protocol          = "tcp"
   security_group_id = aws_security_group.redis.id
+  to_port           = "6379"
+  type              = "ingress"
 }
 
 resource "aws_security_group_rule" "egress" {
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = "6379"
-  to_port           = "6379"
   cidr_blocks       = var.vpc_subnets_cidr_blocks
+  from_port         = "6379"
+  protocol          = "tcp"
   security_group_id = aws_security_group.redis.id
+  to_port           = "6379"
+  type              = "egress"
 }
--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.60"
+      version = ">= 4.00"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.6"
+      version = ">= 2.6"
     }
   }
 }
