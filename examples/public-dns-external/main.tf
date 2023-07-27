87a0d8c808e27f7ab4267a146a2af58b5957898e
George Scott <george.scott@wandb.com>
2023-07-27 11:01:38 -0500

    added comment

--- examples/public-dns-external/main.tf
+++ examples/public-dns-external/main.tf
@@ -26,6 +26,10 @@ module "wandb_infra" {
   database_sort_buffer_size    = var.database_sort_buffer_size
 
   allowed_inbound_cidr      = var.allowed_inbound_cidr
+  /////////////////////////////////////////////////////////////
+  // this defaults to all, which allows the ELB to reached
+  // by IPv6 from any origin. to block IPv6, set this to "::/1".
+  ////////////////////////////////////////////////////////////
   allowed_inbound_ipv6_cidr = ["::/0"]
 
   eks_cluster_version            = "1.24"
