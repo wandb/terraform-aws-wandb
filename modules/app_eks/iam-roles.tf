65585c17b39ebde2ca89d0fa9c0a1632b2caa39e
George Scott <george.scott@wandb.com>
2023-07-27 10:43:33 -0500

    BREAKING: remove inline_policies

--- modules/app_eks/iam-roles.tf
+++ modules/app_eks/iam-roles.tf
@@ -2,8 +2,7 @@ resource "aws_iam_role" "node" {
   name               = "${var.namespace}-node"
   assume_role_policy = data.aws_iam_policy_document.node_assume.json
 
-  // todo: refactor --> v1.16.3
-  inline_policy {}
+
 }
 
 
