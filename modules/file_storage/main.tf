8d3558c1e3ee0e9bdb8782098cbbf0739b4540cd
George Scott <george.scott@wandb.com>
2023-08-03 16:57:42 -0500

    order

--- modules/file_storage/main.tf
+++ modules/file_storage/main.tf
@@ -22,33 +22,6 @@ resource "aws_s3_bucket" "file_storage" {
   depends_on = [aws_sqs_queue.file_storage]
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "file_storage" {
-  bucket = aws_s3_bucket.file_storage.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = var.kms_key_arn
-      sse_algorithm     = var.sse_algorithm
-    }
-  }
-}
-
-
-resource "aws_s3_bucket_public_access_block" "file_storage" {
-  bucket                  = aws_s3_bucket.file_storage.id
-  block_public_acls       = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-  ignore_public_acls      = true
-}
-
-resource "aws_s3_bucket_ownership_controls" "file_storage" {
-  bucket = aws_s3_bucket.file_storage.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
 resource "aws_s3_bucket_acl" "file_storage" {
   depends_on = [aws_s3_bucket_ownership_controls.file_storage]
 
@@ -67,6 +40,34 @@ resource "aws_s3_bucket_cors_configuration" "file_storage" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket_ownership_controls" "file_storage" {
+  bucket = aws_s3_bucket.file_storage.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "file_storage" {
+  bucket                  = aws_s3_bucket.file_storage.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "file_storage" {
+  bucket = aws_s3_bucket.file_storage.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.kms_key_arn
+      sse_algorithm     = var.sse_algorithm
+    }
+  }
+}
+
+
 
 
 # Give the bucket permission to send messages onto the queue. Looks like we
