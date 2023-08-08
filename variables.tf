c0be73b366529c0e0d752452f8e9d5903be9a6d8
George Scott <george.scott@wandb.com>
2023-08-08 17:48:50 -0400

    changes

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "use_internal_queue" {
 variable "database_engine_version" {
   description = "Version for MySQL Auora"
   type        = string
-  default     = "8.0.mysql_aurora.3.03.0"
+  default     = "8.0.mysql_aurora.3.03.2"
 }
 
 variable "database_instance_class" {
