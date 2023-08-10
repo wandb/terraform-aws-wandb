c7dc0c589a387918ea83814fd970d10ba9b8d1af
George Scott <george.scott@wandb.com>
2023-08-10 00:48:49 -0400

    working

--- modules/juicefs/iam-policy-docs.tf
+++ modules/juicefs/iam-policy-docs.tf
@@ -7,5 +7,17 @@ data "aws_iam_policy_document" "juicefs" {
       "${data.aws_s3_bucket.juicefs.arn}/*"
     ]
   }
+
+  statement {
+    actions = [
+     "kms:Decrypt",
+          "kms:Describe*",
+          "kms:Encrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*"
+    ]
+    effect = "Allow"
+    resources = [ "*" ]
+  }
 }
 
--- modules/juicefs/locals.tf
+++ modules/juicefs/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  objectstore_url = "https://${data.aws_s3_bucket.juicefs.bucket_domain_name}/juicefs"
-  metastore_url   = "rediss://${var.elasticache_user}:${var.elasticache_password}@${aws_elasticache_replication_group.juicefs.configuration_endpoint_address}:${aws_elasticache_replication_group.juicefs.port}/1"
+  objectstore_url = "https://${data.aws_s3_bucket.juicefs.bucket_regional_domain_name}"
+  metastore_url   = "rediss://:${var.elasticache_password}@${aws_elasticache_replication_group.juicefs.configuration_endpoint_address}:${aws_elasticache_replication_group.juicefs.port}/1"
 }
