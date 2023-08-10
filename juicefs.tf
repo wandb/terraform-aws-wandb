2100e59ae4c62c6b17ffcb8eb6c7cd2d32e8dc71
George Scott <george.scott@wandb.com>
2023-08-09 20:38:00 -0400

    security group

--- juicefs.tf
+++ juicefs.tf
@@ -3,7 +3,7 @@ module "juicefs" {
 
   k8s_cluster_id = module.app_eks.cluster_id
   namespace          = var.namespace
-  security_group_ids = [module.app_eks.worker_node_security_group_id]
+  security_group_ids = [module.redis[0].security_group_id]
   subnet_group_name  = module.redis[0].subnet_group_name
   subnet_ids         = module.networking.private_subnets
   s3_bucket_name     = module.file_storage[0].bucket_name
