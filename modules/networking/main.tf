85f997655f4119ad1fefe94f07d98f19a9e4ecd9
George Scott <george.scott@wandb.com>
2023-08-16 17:35:19 -0500

    feat: Update vcp provider (#120)
    * feat: Update vcp provider
    
    * Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -4,30 +4,28 @@ data "aws_availability_zones" "available" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
+  version = "~> 4.0"
 
   create_vpc = var.create_vpc
 
-  # VPC
-  name                 = "${var.namespace}-vpc"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  azs                 = data.aws_availability_zones.available.names
-  cidr                = var.cidr
-  private_subnets     = var.private_subnet_cidrs
-  public_subnets      = var.public_subnet_cidrs
-  database_subnets    = var.database_subnet_cidrs
-  elasticache_subnets = var.create_elasticache_subnet ? var.elasticache_subnet_cidrs : []
-
-  enable_nat_gateway = true
-  single_nat_gateway = false
-  enable_vpn_gateway = var.enable_vpn_gateway
-  customer_gateways  = var.customer_gateways
-
-  create_igw = true
-
-  manage_default_security_group  = true
+  azs                            = data.aws_availability_zones.available.names
+  cidr                           = var.cidr
+  create_igw                     = true
+  customer_gateways              = var.customer_gateways
+  database_subnets               = var.database_subnet_cidrs
+  # setting these to empty shouldn't interfere
+  # with SG rules attached to SGs
   default_security_group_egress  = []
   default_security_group_ingress = []
+  elasticache_subnets            = var.create_elasticache_subnet ? var.elasticache_subnet_cidrs : []
+  enable_dns_hostnames           = true
+  enable_dns_support             = true
+  enable_nat_gateway             = true
+  enable_vpn_gateway             = var.enable_vpn_gateway
+  manage_default_security_group  = true
+  map_public_ip_on_launch        = true
+  name                           = "${var.namespace}-vpc"
+  private_subnets                = var.private_subnet_cidrs
+  public_subnets                 = var.public_subnet_cidrs
+  single_nat_gateway             = false
 }
