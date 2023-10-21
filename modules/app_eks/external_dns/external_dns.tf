67c4c710f0d3bc74992f8f3e4a0129eddd4c1d19
Justin Brooks <jsbroks@gmail.com>
2023-10-21 02:40:20 -0400

    feat: external dns

---
+++ modules/app_eks/external_dns/external_dns.tf
@@ -0,0 +1,32 @@
+resource "helm_release" "external_dns" {
+  name       = "external-dns"
+  namespace  = "kube-system"
+  chart      = "external-dns"
+  version    = "1.13.1"
+  repository = "https://kubernetes-sigs.github.io/external-dns"
+
+  set {
+    name  = "rbac.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-dns"
+  }
+
+  set {
+    name = "domainFilters[0]"
+    value = var.fqdn
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.default.arn
+  }
+}
