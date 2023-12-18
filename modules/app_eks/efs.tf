d8a73e648549796dc46c52eece9bf0ba2e0a7463
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2023-12-18 14:19:27 -0600

    adding aws_security_group_rule

--- modules/app_eks/efs.tf
+++ modules/app_eks/efs.tf
@@ -23,6 +23,15 @@ resource "aws_security_group" "storage_class_nfs" {
   vpc_id      = var.network_id
 }
 
+resource "aws_vpc_security_group_ingress_rule" "example" {
+  security_group_id = aws_security_group.example.id
+
+  cidr_ipv4   = "10.0.0.0/8"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
 resource "aws_security_group_rule" "nfs_ingress" {
   description              = "NFS inbound"
   type                     = "ingress"
