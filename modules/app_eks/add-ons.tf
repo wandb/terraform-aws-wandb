803acbdd8c27e9ba1fa25f4a97d73b265ef1745b
George Scott <george.scott@wandb.com>
2024-01-11 13:15:57 -0600

    cleanup

--- modules/app_eks/add-ons.tf
+++ modules/app_eks/add-ons.tf
@@ -1,15 +1,8 @@
-resource "aws_eks_addon" "ebs_csi" {
-  depends_on = [
-    module.eks,
-    aws_eks_addon.vpc_cni
-  ]
-
-  addon_name   = "aws-ebs-csi-driver"
-  addon_version = "v1.25.0-eksbuild.1"
-  cluster_name = var.namespace
-  preserve = false
-  resolve_conflicts = "OVERWRITE"  
-
+#########################################
+# OIDC stuff for VPC CNI
+#########################################
+data "tls_certificate" "vpc_cni" {
+  url = module.eks.cluster_oidc_issuer_url
 }
 
 resource "aws_eks_addon" "vpc_cni" {
@@ -27,20 +20,6 @@ resource "aws_eks_addon" "vpc_cni" {
   service_account_role_arn = aws_iam_role.node.arn
 }
 
-#########################################
-# OIDC stuff for VPC CNI
-#########################################
-data "tls_certificate" "vpc_cni" {
-  url = module.eks.cluster_oidc_issuer_url
-}
 
-#resource "aws_iam_openid_connect_provider" "vpc_cni" {
-#  client_id_list  = ["sts.amazonaws.com"]
-#  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-#  url             = module.eks.cluster_oidc_issuer_url
-#}
 
-resource "aws_iam_role_policy_attachment" "vpc_cni" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node.name
-}
+
