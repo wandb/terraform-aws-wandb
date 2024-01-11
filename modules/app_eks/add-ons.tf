bb5eaa6fa486bb95e15548ca164f97051890da0b
George Scott <george.scott@wandb.com>
2024-01-11 12:53:06 -0600

    install EKS VPC-CNI Add-on

--- modules/app_eks/add-ons.tf
+++ modules/app_eks/add-ons.tf
@@ -1,27 +1,30 @@
 resource "aws_eks_addon" "ebs_csi" {
+  depends_on = [
+    module.eks,
+    aws_eks_addon.vpc_cni
+  ]
+
   addon_name   = "aws-ebs-csi-driver"
   addon_version = "v1.25.0-eksbuild.1"
   cluster_name = var.namespace
   preserve = false
   resolve_conflicts = "OVERWRITE"  
-  depends_on = [
-    module.eks,
-    aws_eks_addon.vpc_cni
-  ]
+
 }
 
 resource "aws_eks_addon" "vpc_cni" {
+  depends_on   = [
+    module.eks,
+    aws_iam_openid_connect_provider.eks,
+    aws_iam_role_policy_attachment.vpc_cni
+  ]
+
   addon_name   = "vpc-cni"
   addon_version = "v1.13.0-eksbuild.1"
   cluster_name = var.namespace
   preserve = false
   resolve_conflicts = "OVERWRITE"
   service_account_role_arn = aws_iam_role.node.arn
-  depends_on   = [
-    module.eks,
-    aws_iam_openid_connect_provider.eks,
-    aws_iam_role_policy_attachment.vpc_cni
-  ]
 }
 
 #########################################
