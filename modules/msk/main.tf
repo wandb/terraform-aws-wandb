c39dbce2915cdc78f7d410fd229b46fa96f7bac0
Justin Brooks <jsbroks@gmail.com>
2024-02-15 11:10:59 -0500

    add variables

--- modules/msk/main.tf
+++ modules/msk/main.tf
@@ -21,18 +21,18 @@ resource "aws_security_group" "msk" {
 
 resource "aws_msk_cluster" "default" {
   cluster_name           = var.namespace
-  kafka_version          = "3.6.0"
+  kafka_version          = var.kafka_version
   number_of_broker_nodes = length(var.private_subnets)
 
   broker_node_group_info {
-    instance_type = "kafka.m5.large"
+    instance_type = var.instance_type
 
     client_subnets  = var.private_subnets
     security_groups = [aws_security_group.msk.id]
 
     storage_info {
       ebs_storage_info {
-        volume_size = 20
+        volume_size = var.volume_size
       }
     }
   }
--- modules/msk/variables.tf
+++ modules/msk/variables.tf
@@ -9,3 +9,18 @@ variable "vpc_id" {
 variable "private_subnets" {
   type = list(string)
 }
+
+variable "instance_type" {
+  type    = string
+  default = "kafka.m5.large"
+}
+
+variable "volume_size" {
+  type    = number
+  default = 20
+}
+
+variable "kafka_version" {
+  type    = string
+  default = "3.6.0"
+}
