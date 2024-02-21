cc239227d6a033b83349690ec495f89172b8d437
George Scott <george.scott@wandb.com>
2024-02-21 16:56:05 -0600

    removed outputs

--- examples/public-dns-external/main.tf
+++ examples/public-dns-external/main.tf
@@ -112,18 +112,6 @@ output "bucket_queue_name" {
   value = module.wandb_infra.bucket_queue_name
 }
 
-output "database_instance_type" {
-  value = module.wandb_infra.database_instance_type
-}
-
-output "eks_node_instance_type" {
-  value = module.wandb_infra.eks_node_instance_type
-}
-
-output "redis_instance_type" {
-  value = module.wandb_infra.redis_instance_type
-}
-
 output "standardized_size" {
   value = var.size
 }
