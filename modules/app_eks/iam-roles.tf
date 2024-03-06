e7b755fef547c55ec1e93a3d0be491dc266a2485
velotioaastha <aastha.gupta@velotio.com>
2024-03-06 13:22:17 +0530

    addressed comments

--- modules/app_eks/iam-roles.tf
+++ modules/app_eks/iam-roles.tf
@@ -4,4 +4,26 @@ resource "aws_iam_role" "node" {
 
 }
 
-
+# IAM Role for IRSA
+resource "aws_iam_role" "irsa_role" {
+  name = "${var.namespace}-irsa-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Sid       = ""
+        Effect    = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${aws_iam_openid_connect_provider.eks.url}"
+        }
+        Action    = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "${aws_iam_openid_connect_provider.eks.url}:sub" = "system:serviceaccount:${var.namespace}:*"
+            "${aws_iam_openid_connect_provider.eks.url}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
