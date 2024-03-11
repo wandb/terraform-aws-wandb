713c1cc233e9ff087fac00e6f9ce83fac2e38eab
Aditya Choudhari <48932219+adityachoudhari26@users.noreply.github.com>
2024-03-11 08:54:47 -0700

    fix: Bump operator chart version (#187)

--- main.tf
+++ main.tf
@@ -228,7 +228,7 @@ module "wandb" {
     module.app_eks,
     module.redis,
   ]
-  operator_chart_version = "1.1.0"
+  operator_chart_version = "1.1.2"
   controller_image_tag   = "1.10.1"
 
   spec = {
