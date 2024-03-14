ae2261116a08028763f43955be6d870deb8bdc22
Nigel Foucha <73838612+nfoucha@users.noreply.github.com>
2024-03-14 16:10:22 -0400

    feat(backend): Make minimum nodes for t-shirts '3' (#188)

--- deployment-size.tf
+++ deployment-size.tf
@@ -7,19 +7,19 @@ locals {
   deployment_size = {
     small = {
       db            = "db.r6g.large",
-      node_count    = 2,
+      node_count    = 3,
       node_instance = "r6i.xlarge"
       cache         = "cache.m6g.large"
     },
     medium = {
       db            = "db.r6g.xlarge",
-      node_count    = 2,
+      node_count    = 3,
       node_instance = "r6i.xlarge"
       cache         = "cache.m6g.large"
     },
     large = {
       db            = "db.r6g.2xlarge",
-      node_count    = 2,
+      node_count    = 3,
       node_instance = "r6i.2xlarge"
       cache         = "cache.m6g.xlarge"
     },
