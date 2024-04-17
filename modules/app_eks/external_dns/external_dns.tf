7adf4203c2b75447def8483a93d972ed42eb69fc
Zachary Blasczyk <77289967+wandb-zacharyblasczyk@users.noreply.github.com>
2024-04-17 10:16:41 -0500

    fix: Adding missing extra_fqdn support for operator that was supported previously (#197)
    * fix: Adding extra_fqdn support for operator
    
    * extreamly cursed code to get around an external dns bug, bumping LB Conrroller and External DNS
    
    * docs update

--- modules/app_eks/external_dns/external_dns.tf
+++ modules/app_eks/external_dns/external_dns.tf
@@ -2,7 +2,7 @@ resource "helm_release" "external_dns" {
   name       = "external-dns"
   namespace  = "kube-system"
   chart      = "external-dns"
-  version    = "1.13.1"
+  version    = "1.14.1"
   repository = "https://kubernetes-sigs.github.io/external-dns"
 
   set {
@@ -34,6 +34,4 @@ resource "helm_release" "external_dns" {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = aws_iam_role.default.arn
   }
-
-
 }
