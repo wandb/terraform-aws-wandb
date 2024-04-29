67fcf85bbe35b63ab86a6f2255b6138be5ce2c3b
George Scott <george.scott@wandb.com>
2024-04-29 10:56:47 -0500

    Update modules/app_eks/add-ons.tf
    Co-authored-by: Justin Brooks <jsbroks@gmail.com>

--- modules/app_eks/add-ons.tf
+++ modules/app_eks/add-ons.tf
@@ -63,7 +63,7 @@ resource "aws_eks_addon" "coredns" {
 
 resource "aws_eks_addon" "kube_proxy" {
   depends_on = [
-    aws_eks_addon.vpc-cni
+    aws_eks_addon.vpc_cni
   ]
   cluster_name                = var.namespace
   addon_name                  = "kube-proxy"
