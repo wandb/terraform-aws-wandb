c7ca6fc2b5d772a84f251056717e16f015932b5e
George Scott <george.scott@wandb.com>
2024-05-23 12:22:10 -0500

    updates

--- modules/app_eks/add-ons.tf
+++ modules/app_eks/add-ons.tf
@@ -47,7 +47,7 @@ resource "aws_eks_addon" "aws_ebs_csi_driver" {
   ]
   cluster_name                = var.namespace
   addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.25.0-eksbuild.1"
+  addon_version               = "v1.31.0-eksbuild.1"
   resolve_conflicts           = "OVERWRITE"
 }
 
@@ -67,14 +67,14 @@ resource "aws_eks_addon" "kube_proxy" {
   ]
   cluster_name                = var.namespace
   addon_name                  = "kube-proxy"
-  addon_version               = "v1.25.14-eksbuild.2"
+  addon_version               = "v1.26.9-eksbuild.2"
   resolve_conflicts           = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name                = var.namespace
   addon_name                  = "vpc-cni"
-  addon_version               = "v1.18.0-eksbuild.1"
+  addon_version               = "v1.18.1-eksbuild.1"
   resolve_conflicts           = "OVERWRITE"
   service_account_role_arn = aws_iam_role.oidc.arn
 }
