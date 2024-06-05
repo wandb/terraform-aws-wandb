bb9ee0bb8400568a665af26471f687c658469255
amanpruthi <aman.pruthi@velotio.com>
2024-06-05 16:20:53 +0530

    feat: added addons version for eks 1.28

--- modules/app_eks/add-ons.tf
+++ modules/app_eks/add-ons.tf
@@ -37,7 +37,7 @@ resource "aws_eks_addon" "aws_efs_csi_driver" {
    ]
    cluster_name               = var.namespace
    addon_name                 = "aws-efs-csi-driver"
-   addon_version              = "v1.7.7-eksbuild.1"
+   addon_version              = "v2.0.3-eksbuild.1"
    resolve_conflicts          = "OVERWRITE"
  }
 
@@ -47,7 +47,7 @@ resource "aws_eks_addon" "aws_ebs_csi_driver" {
   ]
   cluster_name                = var.namespace
   addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.25.0-eksbuild.1"
+  addon_version               = "v1.31.0-eksbuild.1"
   resolve_conflicts           = "OVERWRITE"
 }
 
@@ -57,8 +57,9 @@ resource "aws_eks_addon" "coredns" {
   ]
   cluster_name                = var.namespace
   addon_name                  = "coredns"
-  addon_version               = "v1.9.3-eksbuild.11"
+  addon_version               = "v1.10.1-eksbuild.4"
   resolve_conflicts           = "OVERWRITE"
+  configuration_values = "{\"replicaCount\":2}"
 }
 
 resource "aws_eks_addon" "kube_proxy" {
@@ -67,7 +68,7 @@ resource "aws_eks_addon" "kube_proxy" {
   ]
   cluster_name                = var.namespace
   addon_name                  = "kube-proxy"
-  addon_version               = "v1.25.14-eksbuild.2"
+  addon_version               = "v1.28.2-eksbuild.2"
   resolve_conflicts           = "OVERWRITE"
 }
 
