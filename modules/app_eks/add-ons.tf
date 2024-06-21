0fcd4c8320167fe18099b53c5a217bf18d01e93e
amanpruthi <aman.pruthi@velotio.com>
2024-06-21 10:13:11 +0530

    Added latest addons version

--- modules/app_eks/add-ons.tf
+++ modules/app_eks/add-ons.tf
@@ -36,7 +36,7 @@ resource "aws_eks_addon" "aws_efs_csi_driver" {
    ]
    cluster_name               = var.namespace
    addon_name                 = "aws-efs-csi-driver"
-   addon_version              = "v2.0.3-eksbuild.1"
+   addon_version              = "v2.0.4-eksbuild.1"
    resolve_conflicts          = "OVERWRITE"
 }
 
@@ -56,7 +56,7 @@ resource "aws_eks_addon" "coredns" {
   ]
   cluster_name                = var.namespace
   addon_name                  = "coredns"
-  addon_version               = "v1.10.1-eksbuild.4"
+  addon_version               = "v1.10.1-eksbuild.11"
   resolve_conflicts           = "OVERWRITE"
 }
 
@@ -66,14 +66,14 @@ resource "aws_eks_addon" "kube_proxy" {
   ]
   cluster_name                = var.namespace
   addon_name                  = "kube-proxy"
-  addon_version               = "v1.27.6-eksbuild.2"
+  addon_version               = "v1.27.12-eksbuild.5"
   resolve_conflicts           = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name             = var.namespace
   addon_name               = "vpc-cni"
-  addon_version            = "v1.18.0-eksbuild.1"
+  addon_version            = "v1.18.2-eksbuild.1"
   resolve_conflicts        = "OVERWRITE"
   service_account_role_arn = aws_iam_role.oidc.arn
 }
