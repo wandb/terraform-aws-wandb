535a06993005bd9e667343f223dd5419b0ede4ed
Zachary Blasczyk <77289967+zacharyblasczyk@users.noreply.github.com>
2024-06-27 15:00:44 -0500

    fix: AWS VPC CNI revert

--- modules/app_eks/add-ons.tf
+++ modules/app_eks/add-ons.tf
@@ -74,7 +74,7 @@ resource "aws_eks_addon" "kube_proxy" {
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name             = var.namespace
   addon_name               = "vpc-cni"
-  addon_version            = "v1.18.0-eksbuild.1"
+  addon_version            = "v1.18.2-eksbuild.1"
   resolve_conflicts        = "OVERWRITE"
   service_account_role_arn = aws_iam_role.oidc.arn
 }
