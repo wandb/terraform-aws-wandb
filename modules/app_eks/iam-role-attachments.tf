fdc6dfd90a5b8f5bdb734fcad2c245d524ecc816
amanpruthi <aman.pruthi@velotio.com>
2024-09-27 15:07:35 +0530

    comment policy

--- modules/app_eks/iam-role-attachments.tf
+++ modules/app_eks/iam-role-attachments.tf
@@ -1,7 +1,7 @@
-resource "aws_iam_role_policy_attachment" "node_cloudwatch" {
-  role       = aws_iam_role.node.name
-  policy_arn = aws_iam_policy.node_cloudwatch.arn
-}
+# resource "aws_iam_role_policy_attachment" "node_cloudwatch" {
+#   role       = aws_iam_role.node.name
+#   policy_arn = aws_iam_policy.node_cloudwatch.arn
+# }
 
 resource "aws_iam_role_policy_attachment" "node_IMDSv2" {
   role       = aws_iam_role.node.name
