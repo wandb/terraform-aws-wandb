55d6f21bca612a8f48fc4f45dc5063c591502213
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2024-10-09 21:03:21 -0500

    fix cluster_name

--- modules/app_eks/outputs.tf
+++ modules/app_eks/outputs.tf
@@ -1,7 +1,7 @@
 output "autoscaling_group_names" {
   value = { for name, value in module.eks.node_groups : name => lookup(lookup(lookup(value, "resources")[0], "autoscaling_groups")[0], "name") }
 }
-output "cluster_name" {
+output "cluster_id" {
   value       = module.eks.cluster_id
   description = "ID of the created EKS cluster"
 }
