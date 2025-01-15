a16516ceeecc042145e4018b99e49fefa59e86d5
Shiva Ramamurthi <shiva.ramamurthi@wandb.com>
2025-01-15 14:48:07 -0800

    feat: Add VPC flow logs option

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -45,7 +45,7 @@ variable "create_elasticache_subnet" {
   default     = false
 }
 
-variable "enable_flow_logs" {
+variable "enable_flow_log" {
   description = "Controls whether VPC Flow Logs are enabled"
   type        = bool
   default     = false
