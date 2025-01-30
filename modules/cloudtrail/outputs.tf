9351039b340a27973a5510820a7b3a72f8ab210e
Matthew Hooker <mwhooker@gmail.com>
2025-01-29 16:16:31 -0800

    remove indexing because we removed count

--- modules/cloudtrail/outputs.tf
+++ modules/cloudtrail/outputs.tf
@@ -1,19 +1,19 @@
 output "cloudtrail_bucket_name" {
   description = "Name of the S3 bucket storing CloudTrail logs specific to S3 events"
-  value       = var.enable_cloudtrail_s3_logging ? aws_s3_bucket.cloudtrail_logs[0].bucket : null
+  value       = var.enable_cloudtrail_s3_logging ? aws_s3_bucket.cloudtrail_logs.bucket : null
 }
 
 output "cloudtrail_bucket_arn" {
   description = "ARN of the S3 bucket storing CloudTrail logs specific to S3 events"
-  value       = var.enable_cloudtrail_s3_logging ? aws_s3_bucket.cloudtrail_logs[0].arn : null
+  value       = var.enable_cloudtrail_s3_logging ? aws_s3_bucket.cloudtrail_logs.arn : null
 }
 
 output "cloudtrail_name" {
   description = "Name of the CloudTrail instance"
-  value       = var.enable_cloudtrail_s3_logging ? aws_cloudtrail.s3_event_logs[0].name : null
+  value       = var.enable_cloudtrail_s3_logging ? aws_cloudtrail.s3_event_logs.name : null
 }
 
 output "cloudtrail_arn" {
   description = "ARN of the CloudTrail instance"
-  value       = var.enable_cloudtrail_s3_logging ? aws_cloudtrail.s3_event_logs[0].arn : null
+  value       = var.enable_cloudtrail_s3_logging ? aws_cloudtrail.s3_event_logs.arn : null
 }
