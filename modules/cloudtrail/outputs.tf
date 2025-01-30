6aeb94f88b0f872e8874c0178bceaa12d0773890
shivawandb <167111773+shivawandb@users.noreply.github.com>
2025-01-30 09:42:55 -0800

    feat: Cloudtrail logging for s3 events (#333)
    * feat: Cloudtrail logging for s3 events
    
    * feat: Cloudtrail logging for s3 events

--- modules/cloudtrail/outputs.tf
+++ modules/cloudtrail/outputs.tf
@@ -1,24 +1,19 @@
 output "cloudtrail_bucket_name" {
   description = "Name of the S3 bucket storing CloudTrail logs specific to S3 events"
-  value       = var.enable_cloudtrail_s3_logging ? aws_s3_bucket.cloudtrail_logs[0].bucket : null
+  value       = aws_s3_bucket.cloudtrail_logs.bucket
 }
 
 output "cloudtrail_bucket_arn" {
   description = "ARN of the S3 bucket storing CloudTrail logs specific to S3 events"
-  value       = var.enable_cloudtrail_s3_logging ? aws_s3_bucket.cloudtrail_logs[0].arn : null
+  value       = aws_s3_bucket.cloudtrail_logs.arn
 }
 
 output "cloudtrail_name" {
   description = "Name of the CloudTrail instance"
-  value       = var.enable_cloudtrail_s3_logging ? aws_cloudtrail.s3_event_logs[0].name : null
+  value       = aws_cloudtrail.s3_event_logs.name
 }
 
 output "cloudtrail_arn" {
   description = "ARN of the CloudTrail instance"
-  value       = var.enable_cloudtrail_s3_logging ? aws_cloudtrail.s3_event_logs[0].arn : null
-}
-
-output "force_destroy_warning" {
-  description = "Warning message if force_destroy is enabled"
-  value       = var.force_destroy ? "WARNING: The S3 bucket is configured to be forcefully destroyed." : null
+  value       = aws_cloudtrail.s3_event_logs.arn
 }
