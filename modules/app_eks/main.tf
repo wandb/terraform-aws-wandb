998a33d108f0bad7bdabfa020d78019062a87cca
Ish Shah <ish.shah@wandb.com>
2025-02-25 16:05:42 -0800

    fix: ignore tag changes to customer-ns after instantiation

--- modules/app_eks/main.tf
+++ modules/app_eks/main.tf
@@ -80,6 +80,10 @@ module "eks" {
     TerraformNamespace = var.namespace
     TerraformModule    = "terraform-aws-wandb/module/app_eks"
   })
+  
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
 
 resource "kubernetes_annotations" "gp2" {
