db1e25d51f4f41a54f78b1039f6c7f19a275232e
Daniel Panzella <daniel.panzella@gmail.com>
2025-05-14 12:16:28 -0700

    fix: Add explicit dependencies so TF destroy resources in the correct order. (#356)
    * fix: Add explicit dependencies so TF destroy resources in the correct order.
    
    * fix: Additional dependencies and a sleep after destroying the application to give everything time to cleanup in the cluster
    
    * chore: fmt and add time to required providers
    
    * chore: newline at end of file
    
    * chore: fmt

--- main.tf
+++ main.tf
@@ -194,6 +194,10 @@ module "app_eks" {
   eks_addon_metrics_server_version = var.eks_addon_metrics_server_version
 
   cache_size = var.cache_size
+
+  depends_on = [
+    module.networking,
+  ]
 }
 
 
@@ -406,14 +410,23 @@ locals {
   }
 }
 
+resource "time_sleep" "wait_for_deletion_reconcile" {
+  depends_on = [module.app_eks]
+
+  # external-dns has a 5m reconcile timer, so we need to give it time to clean up the DNS records
+  destroy_duration = "5m"
+}
+
 module "wandb" {
   source  = "wandb/wandb/helm"
   version = "3.0.0"
 
   depends_on = [
     module.database,
-    module.app_eks,
     module.redis,
+    module.acm,
+    module.networking.public_subnets,
+    time_sleep.wait_for_deletion_reconcile
   ]
 
   operator_chart_version = var.operator_chart_version
