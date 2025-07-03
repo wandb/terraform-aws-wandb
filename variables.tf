244b70d0ce9ea45b0715b75d90d51501f7a27897
Daniel Panzella <daniel.panzella@gmail.com>
2025-07-03 13:17:12 -0700

    fix: Bump operator chart and image versions to latest (#358)

--- variables.tf
+++ variables.tf
@@ -29,13 +29,13 @@ variable "size" {
 variable "operator_chart_version" {
   type        = string
   description = "Version of the operator chart to deploy"
-  default     = "1.3.4"
+  default     = "1.4.2"
 }
 
 variable "controller_image_tag" {
   type        = string
   description = "Tag of the controller image to deploy"
-  default     = "1.14.0"
+  default     = "1.20.0"
 }
 
 variable "enable_helm_operator" {
