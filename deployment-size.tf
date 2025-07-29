922c45a9f9e72d3eb251fda98f1aa61e66cb9fc2
Daniel Panzella <daniel.panzella@gmail.com>
2025-07-29 06:19:34 -0700

    fix: Increase instance sizes on all sizes up to large (#360)

--- deployment-size.tf
+++ deployment-size.tf
@@ -9,28 +9,28 @@ locals {
       db               = "db.r6g.large",
       min_nodes_per_az = 1,
       max_nodes_per_az = 4,
-      node_instance    = "r6i.xlarge"
+      node_instance    = "r6i.2xlarge"
       cache            = "cache.m6g.large"
     },
     medium = {
       db               = "db.r6g.xlarge",
       min_nodes_per_az = 1,
       max_nodes_per_az = 4,
-      node_instance    = "r6i.xlarge"
+      node_instance    = "r6i.4xlarge"
       cache            = "cache.m6g.large"
     },
     large = {
       db               = "db.r6g.2xlarge",
       min_nodes_per_az = 1,
       max_nodes_per_az = 4,
-      node_instance    = "r6i.2xlarge"
+      node_instance    = "r6i.4xlarge"
       cache            = "cache.m6g.xlarge"
     },
     xlarge = {
       db               = "db.r6g.4xlarge",
       min_nodes_per_az = 1,
       max_nodes_per_az = 4,
-      node_instance    = "r6i.2xlarge"
+      node_instance    = "r6i.4xlarge"
       cache            = "cache.m6g.xlarge"
     },
     xxlarge = {
