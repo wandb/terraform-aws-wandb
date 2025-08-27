738ffacd0a60305eb21f05c6d8bb5e0a2a0d41bd
Jonathan Meeks <jonathan.meeks@wandb.com>
2025-08-27 15:05:07 -0500

    test string interpolation

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -117,7 +117,7 @@ resource "aws_kms_key" "clickhouse_key" {
         "Principal" : {
           "AWS" : [
             "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/prod-kms-request-role",
-            var.clickhouse_tde_arn
+            "${var.clickhouse_tde_arn}"
           ]
         },
         "Action" : [
