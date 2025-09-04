88648def32a5b2be5dfe059397c037cf34d28cbc
Jonathan Meeks <jonathan.meeks@wandb.com>
2025-09-04 09:26:45 -0500

    constrain use_*_redis to at most 1, pt 3

--- main.tf
+++ main.tf
@@ -454,6 +454,21 @@ module "wandb" {
   spec = local.spec
 }
 
+resource "null_resource" "use_redis_validation" {
+  triggers = {
+    use_ctrlplane_redis = var.use_ctrlplane_redis
+    use_chainguard_redis = var.use_chainguard_redis
+    use_external_redis = var.use_external_redis
+  }
+
+  lifecycle {
+    precondition {
+      condition     = (var.use_ctrlplane_redis ? 1 : 0) + (var.use_chainguard_redis ? 1 : 0) + (var.use_external_redis ? 1 : 0) <= 1
+      error_message = "Enable at most one of: use_ctrlplane_redis, use_chainguard_redis, use_external_redis."
+    }
+  }
+}
+
 moved {
   from = module.file_storage[0]
   to   = module.file_storage
